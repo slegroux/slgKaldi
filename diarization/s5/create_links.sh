28533dda4b33ea8920d8d21ebd10f04d8c894cc8
Sylvain Le Groux <sylvainlg@voicera.ai>
2020-02-12 15:47:03 -0800

    add xvector extraction and scoring

--- diarization/s5/create_links.sh
+++ diarization/s5/create_links.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
 . path.sh
-ln -s $KALDI_ROOT/egs/wsj/s5/steps steps
-ln -s $KALDI_ROOT/egs/wsj/s5/utils utils
-ln -s $KALDI_ROOT/egs/sre08/v1/sid sid
+ln -sf $KALDI_ROOT/egs/wsj/s5/steps steps
+ln -sf $KALDI_ROOT/egs/wsj/s5/utils utils
+ln -sf $KALDI_ROOT/egs/sre08/v1/sid sid
+ln -sf $KALDI_ROOT/egs/callhome_diarization/v1/diarization diarization
   
--- diarization/s5/run.sh
+++ diarization/s5/run.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
+
+# kaldi diarization tutorial
 # 2020 sylvain.legroux@gmail.com
 
+set -x
 . path.sh
 
 stage=0
@@ -11,7 +14,7 @@ vaddir=mfcc
 
 . ./utils/parse_options.sh
 
-np=$(( nproc - 1 ))
+np=$(( $(nproc) - 1 ))
 
 set -x
 
@@ -22,25 +25,53 @@ if [ $stage -eq 0 ]; then
     ./utils/fix_data_dir.sh data/$dataset
 fi
 
-# feature extraction
-if [ $stage -eq 1 ]; then
+# set up parallel computations
+if [ -f data/$dataset/utt2spk ]; then
     n_utts=$(cat data/$dataset/utt2spk |wc -l)
-    if [ $np -lt $n_utts ]; then
+    if (( $np > $n_utts )); then
         nj=$n_utts
     else
         nj=$np
     fi
+else
+    echo "need utt2spk file. run stage 0"
+fi
 
+# feature extraction
+if [ $stage -eq 1 ]; then
     steps/make_mfcc.sh --mfcc-config conf/mfcc.conf --nj $nj \
-      --cmd "run.pl" \
+      --cmd "run.pl" --write-utt2num-frames true \
       data/$dataset exp/make_mfcc $mfccdir
+    steps/compute_cmvn_stats.sh data/$dataset exp/make_mfcc $mfccdir
     utils/fix_data_dir.sh data/$dataset
 fi
 
 # vad
 if [ $stage -eq 2 ]; then
-    sid/compute_vad_decision.sh --nj $(( nproc -1 )) --cmd "run.pl" \
+    sid/compute_vad_decision.sh --nj $nj --cmd "run.pl" \
         --vad-config conf/vad.conf \
         data/$dataset exp/make_vad $vaddir
-    # utils/fix_data_dir.sh data/$dataset
+    utils/fix_data_dir.sh data/$dataset
+fi
+
+# segment
+if [ $stage -eq 3 ]; then
+    diarization/vad_to_segments.sh --nj $nj --cmd "run.pl" \
+    data/$dataset data/${dataset}_segmented
+    utils/fix_data_dir.sh data/${dataset}_segmented
+fi
+
+# xvectors
+if [ $stage -eq 4 ]; then
+    diarization/nnet3/xvector/extract_xvectors.sh --nj $nj --cmd "run.pl" \
+        --window 1.5 --period 0.75 --apply-cmn false --min-segment 0.5 \
+        $nnet_dir $cmn_dir $nnet_dir/exp/xvectors
+fi
+
+# scoring
+if [ $stage -eq 5 ]; then
+    diarization/nnet3/xvector/score_plda.sh \
+        --cmd "run.pl" \
+        --target-energy 0.9 --nj 20 $nnet_dir/xvectors_sre_combined/ \
+        $nnet_dir/xvectors $nnet_dir/xvectors/plda_scores
 fi
