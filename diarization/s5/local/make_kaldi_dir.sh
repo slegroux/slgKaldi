c97edb5a961278a544111244d165da03088a3ba6
Sylvain Le Groux <sylvainlg@voicera.ai>
2020-02-12 14:35:29 -0800

    add local scripts

---
+++ diarization/s5/local/make_kaldi_dir.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+# 2020 sylvain.legroux@gmail.com
+
+#set -x
+
+audio_dir=$1
+model_sr=8000
+dataset=test
+
+. utils/parse_options.sh
+
+if [ -f data/$dataset/wav.scp ]; then
+    echo "wav.scp already exists, archiving"
+    mv data/$dataset/wav.scp data/$dataset/wav.scp.bu
+fi
+
+if [ -f data/$dataset/utt2spk ]; then
+    echo "utt2spk already exists, archiving"
+    mv data/$dataset/utt2spk data/$dataset/utt2spk.bu
+fi
+
+for i in $audio_dir/*.wav; do
+    if [ $(soxi -sr $i) -ne $model_sr ]; then
+        path="sox $(realpath $i) -r $model_sr -c 1 -e signed -b 16 -t wav - |"
+    else
+        path=$(realpath $i)
+    fi
+    id=$(basename $i .wav)
+    echo $id $path >> data/$dataset/wav.scp
+    echo $id $id >> data/$dataset/utt2spk
+done
+
