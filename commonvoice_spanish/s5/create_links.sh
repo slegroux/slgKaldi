415ad5d5f5cb4d1772f67719eef6268842f4d0bc
Sylvain Le Groux <sylvainlg@voicera.ai>
2020-02-14 13:09:25 -0800

    rnnlm

--- commonvoice_spanish/s5/create_links.sh
+++ commonvoice_spanish/s5/create_links.sh
@@ -5,4 +5,5 @@ ln -sf $KALDI_ROOT/egs/wsj/s5/steps steps
 ln -sf $KALDI_ROOT/egs/wsj/s5/utils utils
 ln -sf $KALDI_ROOT/egs/sre08/v1/sid sid
 ln -sf $KALDI_ROOT/egs/callhome_diarization/v1/diarization diarization
+ln -sf $KALDI_ROOT/egs/wsj/s5/rnnlm
   
