3ac7f642674a2b5a4f88517a5aaa80cd3793a148
DeepLearning VM <google-dl-platform@googlegroups.com>
2020-02-19 00:40:11 +0000

    ivector njob updated

--- commonvoice_spanish/s5/7a_ivector_testing.sh
+++ commonvoice_spanish/s5/7a_ivector_testing.sh
@@ -17,20 +17,20 @@ echo "$0 $@"  # Print the command line for logging
 
 n_speakers_test=$(cat data/${test_set}/spk2utt| wc -l)
 
-utils/copy_data_dir.sh data/$test_set data/${test_set}_hires
-steps/make_mfcc.sh --nj $n_speakers_test --mfcc-config conf/mfcc_hires.conf \
-  --cmd "$train_cmd" data/${test_set}_hires || exit 1;
-steps/compute_cmvn_stats.sh data/${test_set}_hires || exit 1;
-utils/fix_data_dir.sh data/${test_set}_hires || exit 1;
-
-# Also extract iVectors for the test data, but in this case we don't need the speed
-# perturbation (sp).
 if [ $njobs -le $n_speakers_test ]; then
   nj=$njobs
 else
   nj=$n_speakers_test
 fi
 
+utils/copy_data_dir.sh data/$test_set data/${test_set}_hires
+steps/make_mfcc.sh --nj $nj --mfcc-config conf/mfcc_hires.conf \
+  --cmd "$train_cmd" data/${test_set}_hires || exit 1;
+steps/compute_cmvn_stats.sh data/${test_set}_hires || exit 1;
+utils/fix_data_dir.sh data/${test_set}_hires || exit 1;
+
+# Also extract iVectors for the test data, but in this case we don't need the speed
+# perturbation (sp).
 steps/online/nnet2/extract_ivectors_online.sh --cmd "$train_cmd" --nj $nj \
   data/${test_set}_hires exp/nnet3${nnet3_affix}/extractor \
   exp/nnet3${nnet3_affix}/ivectors_${test_set}_hires
