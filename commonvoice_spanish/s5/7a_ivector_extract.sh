e7e4ed79c3d9e7d883b06c5f657582413086cf54
DeepLearning VM <google-dl-platform@googlegroups.com>
2020-02-25 22:53:02 +0000

    i & xvector scripts

---
+++ commonvoice_spanish/s5/7a_ivector_extract.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# Set -e here so that we catch if any executable fails immediately
+set -euo pipefail
+
+njobs=$(($(nproc)-1))
+
+stage=0
+nnet3_affix=_online_cmn
+ivector_extractor=exp/nnet3${nnet3_affix}/extractor
+
+echo "$0 $@"  # Print the command line for logging
+
+. cmd.sh
+. path.sh
+. utils/parse_options.sh
+
+dataset=$1
+ivector_dir=data/${dataset}_hires/ivectors
+n_speakers_test=$(cat data/${dataset}/spk2utt| wc -l)
+
+if [ $njobs -le $n_speakers_test ]; then
+  nj=$njobs
+else
+  nj=$n_speakers_test
+fi
+
+if [ $stage -le 0 ]; then
+  echo "ivector high res mfcc in dataset_hires"
+  utils/copy_data_dir.sh data/$dataset data/${dataset}_hires
+  steps/make_mfcc.sh --nj $nj --mfcc-config conf/mfcc_hires.conf \
+    --cmd "run.pl" data/${dataset}_hires
+  steps/compute_cmvn_stats.sh data/${dataset}_hires
+  utils/fix_data_dir.sh data/${dataset}_hires
+fi
+
+
+if [ $stage -le 1 ]; then
+  steps/online/nnet2/extract_ivectors_online.sh --cmd "run.pl" --nj 40 \
+    data/${dataset}_hires $ivector_extractor $ivector_dir
+fi
---
+++ commonvoice_spanish/s5/7d_concat_xivectors.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+# Set -e here so that we catch if any executable fails immediately
+set -euo pipefail
+
+njobs=$(($(nproc)-1))
+
+stage=0
+
+echo "$0 $@"  # Print the command line for logging
+
+. ./cmd.sh
+. ./path.sh
+. ./utils/parse_options.sh
+
+#input
+ivector_dir=$1
+xvector_dir=$2
+#output
+xivector_dir=$3
+
+# #input
+# ivector_dir=data/${dataset}_hires/ivectors
+# xvector_dir=data/${dataset}_x/xvectors
+# #output
+# xivector_dir=data/${dataset}_x/xi_vectors
+
+if [ $stage -le 0 ]; then
+    # append to ivectors
+    mkdir -p $xivector_dir
+    cp $ivector_dir/ivector_period $xivector_dir
+    
+    append-vector-to-feats scp:$ivector_dir/ivector_online.scp \
+                scp:$xvector_dir/xvector.scp \
+                ark,scp:$xivector_dir/ivectors.ark,$xivector_dir/ivector_online.scp
+
+fi 
