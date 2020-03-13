c4f95bb63c1e7713511625d89c82c485d5c7bca7
Sylvain Le Groux <sylvainlg@voicera.ai>
2020-03-12 22:36:12 -0700

    decouple sp & mfcc

--- commonvoice_spanish/s5/data_prep.sh
+++ commonvoice_spanish/s5/data_prep.sh
@@ -7,8 +7,8 @@ data_root=$DATA
 
 dataset=$1
 
-# ./local/make_sp.sh ${dataset}
-# ./local/make_vp.sh ${dataset}
-# ./local/make_rvb.sh --rir_dir ${data_root}/RIRS_NOISES ${dataset}
-# ./local/make_noise_music_babble.sh --musan_dir ${data_root}/musan ${dataset}
+./local/make_sp.sh ${dataset}
+./local/make_vp.sh ${dataset}
+./local/make_rvb.sh --rir_dir ${data_root}/RIRS_NOISES ${dataset}
+./local/make_noise_music_babble.sh --musan_dir ${data_root}/musan ${dataset}
 ./utils/combine_data.sh data/${dataset}_combined data/${dataset}{_sp,_vp,_rvb,_aug}
