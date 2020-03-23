2981b12ad9e477837daeaef2261c2e567ffb6f41
DeepLearning VM <google-dl-platform@googlegroups.com>
2020-03-23 16:41:49 +0000

    update xvector pipeline

--- commonvoice_spanish/s5/train_nn.sh
+++ commonvoice_spanish/s5/train_nn.sh
@@ -1,42 +1,51 @@
 #!/usr/bin/env bash
 # 2020 slegroux@ccrma.stanford.edu
 
+set -euo pipefail
+
 stage=0
 
-train_set=train_sp_vp
-train_lores=train_sp
-train_ivector_dir=data/${train_set}_hires/ivectors
-xvector_dir=data/${train_set}_x/xvectors
-xivector_dir=data/${train_set}_x/xivectors
+train_set=train_combined
 train_data_dir=data/${train_set}_hires
-ivector_extractor=exp/nnet3_${train_set}/extractor
+train_lores=train_sp
+test_set=test
+train_ivector_dataset=train_sp_vp
+
+train_ivector_dir=data/${train_set}_hires/ivectors
+ivector_extractor=exp/nnet3_${train_ivector_dataset}/extractor
+
+train_xvector_dir=data/${train_set}_x/xvectors
+xivector_dir=data/${train_set}_x/xivectors
+xvector_extractor=$DATA/voxceleb/0007_voxceleb_v2_1a/exp/xvector_nnet_1a
+
 tree=exp/chain/tree_train_sp
-lat_dir=data/train_sp/tri3b_lats
+lat_dir=data/${train_set}/tri3b_lats
 #ivector_dir=data/${train_set}_hires/ivectors
 model_dir=exp/chain/tdnnf_tedlium_${train_set}
-model_dir_xvec=exp/chain/tdnnf_tedlium_train_sp_vp_xvec
-test_set=test
-graph=exp/chain/tree_train_sp/graph_tgsmall
+model_dir_xvec=exp/chain/tdnnf_tedlium_${train_set}_xvec
+
+graph=exp/chain/tree_${train_lores}/graph_tgsmall
+num_epochs=30
+train_stage=354
+
 
 . cmd.sh
 . path.sh
 . utils/parse_options.sh 
 
-
-
 # train ivectors
 if [ $stage -le 0 ]; then
-    ./7_ivector_training.sh --train_set ${train_set}
+    ./7_ivector_training.sh --train_set ${train_set} 
 fi
 
 if [ $stage -le 1 ]; then
     # if training on non sp_vp_hires need to compute mfcc feats
     ./7a_ivector_extract.sh --ivector_extractor $ivector_extractor \
-        $train_set
+        $train_set 
 fi
 
 if [ $stage -le 2 ]; then
-    ./8_dnn_prep.sh --train_set $train_lores --train_ivector_dir $train_ivector_dir
+    ./8_dnn_prep.sh --stage 7 --train_set $train_set --train_ivector_dir $train_ivector_dir || exit 1
 fi
 
 if [ $stage -le 3 ]; then
@@ -44,11 +53,9 @@ if [ $stage -le 3 ]; then
         --tree-dir $tree #--stage 12 || exit 1
 fi
 
-
-
 if [ $stage -le 4 ]; then
-    ./10_dnn_training.sh --dir $model_dir --train_set ${train_set} --tree_dir $tree \
-        --lat_dir $lat_dir --train_ivector_dir $train_ivector_dir --train_stage 315 || exit 1
+    ./10_dnn_training.sh --train_stage -10 --num_epochs $num_epochs --dir $model_dir --train_set ${train_set} --tree_dir $tree \
+        --lat_dir $lat_dir --train_ivector_dir $train_ivector_dir 
 fi
 
 if [ $stage -le 5 ]; then
@@ -58,23 +65,36 @@ if [ $stage -le 5 ]; then
         --ivector_extractor $ivector_extractor $test_set
 fi
 
-if [ $stage -eq 111 ]; then
-    ./7c_xvector_extract.sh $train_set
-    ./7d_concat_xivectors.sh $train_ivector_dir $xvector_dir $xivector_dir
+if [ $stage -le 6 ]; then
+    ./13_prepare_online_decoding.sh --stage 0 --model $model_dir --online_model ${model_dir}_online \
+        --extractor $ivector_extractor --graph $graph --test_set test35
 fi
 
-if [ $stage -eq 112 ]; then
-    ./10_dnn_training.sh --train_set $train_set --dir $model_dir_xvec --tree_dir $tree \
-        --lat_dir $lat_dir --train_data_dir $train_data_dir --train_ivector_dir $xivector_dir --train_stage 77
+# exit 1
+# XVector training
+
+if [ $stage -le 110 ]; then
+    ./9_tdnnf_tedlium_s5_r3_xvec.sh --dir $model_dir_xvec \
+        --tree-dir $tree #--stage 12 || exit 1
 fi
 
-if [ $stage -eq 113 ]; then
+if [ $stage -le 111 ]; then
+    ./7c_xvector_extract.sh --xvector_extractor $xvector_extractor $train_set
+    ./7d_concat_xivectors.sh $train_ivector_dir $train_xvector_dir $xivector_dir
+fi
+
+if [ $stage -le 112 ]; then
+    ./10_dnn_training.sh --train_stage $train_stage --num_epochs $num_epochs --train_set $train_set --dir $model_dir_xvec --tree_dir $tree \
+        --lat_dir $lat_dir --train_data_dir $train_data_dir --train_ivector_dir $xivector_dir
+fi
+
+if [ $stage -le 113 ]; then
     ./10b_dnn_xvector_testing.sh --dir $model_dir_xvec --tree_dir $tree \
-        --ivector_extractor $ivector_extractor $test_set
+        --ivector_extractor $ivector_extractor --xvector_extractor $xvector_extractor \
+        ${test_set}
 fi
 
 if [ $stage -eq 114 ]; then
     ./13_prepare_online_decoding.sh --stage 0 --model $model_dir_xvec --online_model ${model_dir_xvec}_online \
-        --extractor $ivector_extractor --graph $graph --test_set $test_set
-
+        --extractor $ivector_extractor --graph $graph --test_set test35
 fi
