b83a4f1593e1dedd6a1dd8351b435abfaa90d6c2
Sylvain Le Groux <slegroux@ccrma.stanford.edu>
2020-03-31 21:49:02 +0000

    allows access to default kaldi binaries

--- commonvoice_spanish/s5/create_links.sh
+++ commonvoice_spanish/s5/create_links.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/env bash
 
 . path.sh
-ln -sf $KALDI_ROOT/egs/wsj/s5/steps steps
-ln -sf $KALDI_ROOT/egs/wsj/s5/utils utils
-ln -sf $KALDI_ROOT/egs/sre08/v1/sid sid
-ln -sf $KALDI_ROOT/egs/callhome_diarization/v1/diarization diarization
-ln -sf $KALDI_ROOT/egs/wsj/s5/rnnlm
+ln -s $KALDI_ROOT/egs/wsj/s5/steps steps
+ln -s $KALDI_ROOT/egs/wsj/s5/utils utils
   
