c55f0a64b238f4d7609f3ef88d982d2dbbb01b59
Sylvain Le Groux <slegroux@ccrma.stanford.edu>
2020-03-31 22:14:19 +0000

    links to tools

--- commonvoice_spanish/s5/create_links.sh
+++ commonvoice_spanish/s5/create_links.sh
@@ -2,6 +2,12 @@
 
 . path.sh
 
+for i in {steps,utils,sid,diarization,rnnlm}; do
+    if [ -L $i ]; then
+	rm $i
+    fi
+done
+
 ln -s $KALDI_ROOT/egs/wsj/s5/steps steps
 ln -s $KALDI_ROOT/egs/wsj/s5/utils utils
 ln -sf $KALDI_ROOT/egs/sre08/v1/sid sid
