8ed2eebf0c481babcc66f5215df8499e35d66246
Sylvain Le Groux <slegroux@ccrma.stanford.edu>
2020-09-25 00:48:17 +0000

    re-add scripts under main dir

--- commonvoice_spanish/s5/2_test_feature_extraction.sh
+++ commonvoice_spanish/s5/2_test_feature_extraction.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 # (c) 2020 Sylvain Le Groux <slegroux@ccrma.stanford.edu>
 
-./2_feature_extraction.sh --feature_type "mfcc" data/train_50
-./2_feature_extraction.sh --feature_type "plp" data/train_50
+train_set=data/train_500
+
+./2_feature_extraction.sh --feature_type "mfcc" $train_set
+./2_feature_extraction.sh --feature_type "plp" $train_set
--- commonvoice_spanish/s5/3_test_monophone_training.sh
+++ commonvoice_spanish/s5/3_test_monophone_training.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 # (c) 2020 Sylvain Le Groux <slegroux@ccrma.stanford.edu>
 
-./3_monophone_training.sh data/train_500 data/lang exp/mono_500
-# ./3a_am_testing.sh --mono true --compile_graph false data/test35 data/lang_test exp/mono_500 exp/mono_500/graph
+train_set=data/train_500
+test_set=data/test35
+lang=data/lang
+lang_test=data/lang_test
+mono=exp/mono_500
+
+./3_monophone_training.sh ${train_set} ${lang} ${mono}
+./3a_am_testing.sh --mono true --compile_graph false ${test_set} ${lang_test} ${mono} ${mono}/graph
