4a079ad2b1569b11679bcd563eeffe755bfdb56b
Sylvain Le Groux <slegroux@ccrma.stanford.edu>
2020-10-18 01:27:31 +0000

    en lexicon

--- asr/s5/data_prep/make_L.sh
+++ asr/s5/data_prep/make_L.sh
@@ -14,19 +14,18 @@ lang_dir=$3
 
 tmp=$(mktemp -d /tmp/lang-XXXX) #data/local/lang
 
-log_info "Prepare dict"
+
 if [ $lang == 'es' ]; then
-    log_info "spanish"
+    log_info "Prepare ES dict"
     log_time ./data_prep/es_prepare_dict.sh --unk ${unk} ${lexicon} ${dict}
 elif [ $lang == 'en' ]; then
-    log_info "english"
+    log_info "Prepare EN dict"
     log_time ./data_prep/en_prepare_dict.sh --unk ${unk} ${lexicon} ${dict}
 else
-    echo "[ERROR] language not supported"
+    log_err "Language not supported"
 fi
 
 log_info "Prepare lang"
 log_time ./utils/prepare_lang.sh \
     ${dict} ${unk} \
     ${tmp} ${lang_dir}
-
