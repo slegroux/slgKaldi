6595039bf64b5755796a5033be777170d8da89d3
Sylvain Le Groux <slegroux@ccrma.stanford.edu>
2020-10-18 01:28:54 +0000

    update training

--- asr/s5/run_exp.sh
+++ asr/s5/run_exp.sh
@@ -4,8 +4,6 @@
 
 stage=0
 
-# . cfg/mini_librispeech
-
 . utils.sh
 . path.sh
 . utils/parse_options.sh
@@ -21,10 +19,12 @@ if [ $stage -eq 0 ]; then
     #     name=$(basename $dataset .tsv)
     #     if [ ! -d ${data}/${name} ]; then
     #         ./data_prep/format_commonvoice.py ${dataset} ${data}/${name} ||exit 1
+    #         ./utils/fix_data_dir.sh ${data}/${name}
     #     fi
     # done
 
     # librispeech
+    log_info "Data Kaldi formatting"
     for dataset in ${datasets}; do
         name=$(echo $(basename $dataset) | sed 's:-:_:g')
         if [ ! -d ${data}/${name} ]; then
@@ -42,6 +42,7 @@ if [ $stage -eq 1 ]; then
     echo "vocabulary size: " $(cat ${lang_dir}/words.txt|wc -l)
 fi
 
+
 # G
 #  https://kaldi-asr.org/doc/data_prep.html#data_prep_grammar
 # TODO(slg): download corpus + pocolm
@@ -56,11 +57,12 @@ if [ $stage -eq 2 ]; then
 
     ./lm/make_pocolm.sh --order ${lm_order} --limit_unk_history ${limit_unk_history} \
         ${lm_train} ${lm_dev} ${lm_dir}
+
+    log_info "Convert LM to FST"
     utils/format_lm.sh \
         ${lang_dir} ${lm_dir}/${lm_order}gram_unpruned.arpa.gz ${dict}/lexicon.txt \
         ${lm}
 fi
-
 
 # FEATURES
 if [ $stage -eq 3 ]; then
@@ -70,18 +72,23 @@ if [ $stage -eq 3 ]; then
 fi
 
 # HMM-GMM
-if [ $stage -le 4 ]; then
+if [ $stage -eq 4 ]; then
     # TODO(slg): figure out nj settings
     ./hmm/monophone_training.sh --nj ${nj_mono} --boost-silence ${boost_silence} --subset ${subset} ${train} ${lm} ${mono} || exit 1
     ./hmm/triphone_training.sh --boost-silence ${boost_silence} ${train} ${lm} ${mono}_ali ${tri1} || exit 1
     ./hmm/lda_mllt_training.sh ${train} ${lm} ${tri1}_ali ${tri2} || exit 1
     ./hmm/sat_training.sh ${train} ${lm} ${tri2}_ali ${tri3} || exit 1
-    # ./utils/data/subset_data_dir.sh ${test} 1000 ${test}_1000
-    # ./hmm/am_testing.sh --mono false --compile_graph true ${test}_1000 ${lm} ${tri3} ${tri3}/graph_test_1000 || exit 1
 fi
 
+# HMM testing
+if [ $stage -eq 41 ]; then
+    ./utils/data/subset_data_dir.sh ${test} 1000 ${test}_1000
+    ./hmm/am_testing.sh --mono false --compile_graph true ${test}_1000 ${lm} ${tri3} ${tri3}/graph_test_1000 || exit 1
+fi
+
+
 # i-vector training & extract
-if [ $stage -le 5 ]; then
+if [ $stage -eq 5 ]; then
     # data augment end extract hires mfcc
     # input: ${dataset} => output: ${dataset]_sp tri3_sp_ali ${dataset}_sp_vp_hires
     ./embeddings/ivector_data_prep.sh ${train} ${lang_dir} ${tri3}
@@ -91,32 +98,34 @@ if [ $stage -le 5 ]; then
 fi
 
 # pre-trained i-vectors
-if [ $stage -le 6 ]; then
+if [ $stage -eq 6 ]; then
+    # TODO(slg): why do we pertub ? should it just be hires and that's it for computing i-vector and decoding?
     ./data_augment/make_sp_vp_hires.sh ${train} # only needed if using pre-trained i-vector extractor. else stage 5 will provide it
     ./embeddings/ivector_extract.sh ${train}_sp_vp_hires ${ivec_extractor} ${train}_sp_vp_hires/ivectors || exit 1
 fi
 
-if [ $stage -le 7 ]; then
+if [ $stage -eq 7 ]; then
     # use lores (_sp) implicitely align on _sp and generate align lats on _sp_lats
-    # ./dnn/make_lang_chain.sh ${train}_sp ${tri3} ${lang_dir} ${lang_chain} ${tree}
-    # ./dnn/tdnnf_tedlium_s5_r3.sh ${tree} ${mdl}
+    ./dnn/make_lang_chain.sh ${train}_sp ${tri3} ${lang_dir} ${lang_chain} ${tree}
+    # build network architecture
+    ./dnn/${dnn_architecture}.sh ${tree} ${mdl}
     # train on hires (_sp_vp_hires)
     ./dnn/dnn_training.sh --train_stage ${train_stage} --n_gpu ${n_gpu} --num_epochs ${num_epochs} --remove_egs ${remove_egs} \
         ${train}_sp_vp_hires ${lat_dir} ${ivec_data} ${tree} ${mdl}
-    # ./dnn/plot_error_curve.py ${mdl}/accuracy.report
+    ./eval/plot_error_curve.py ${mdl}/accuracy.report
 fi
 
-if [ $stage -le 8 ]; then
+if [ $stage -eq 8 ]; then
     # extract feats for test set
     utils/copy_data_dir.sh ${test} ${test}_hires
     ./features/feature_extract.sh --feature_type "mfcc" --mfcc_config conf/mfcc_hires.conf ${test}_hires
     ./embeddings/ivector_extract.sh ${test}_hires ${ivec_extractor} ${test}_hires/ivectors
     # decode folder: decode_"lm_name"_"test_data"
     ./dnn/dnn_testing.sh --compute_graph true ${test}_hires ${lang_test} ${tree} ${graph} ${test}_hires/ivectors ${mdl} ${decode_test_name}
-    ./dnn/dnn_testing.sh --compute_graph false ${test}_hires ${lang_test} ${tree} ${graph} ${test}_hires/ivectors ${mdl} ${decode_test_name}
+    # ./dnn/dnn_testing.sh --compute_graph false ${test}_hires ${lang_test} ${tree} ${graph} ${test}_hires/ivectors ${mdl} ${decode_test_name}
 fi
 
-if [ $stage -le 9 ]; then
+if [ $stage -eq 9 ]; then
     if [ ! -d ${rnnlm_data} ]; then
         mkdir -p ${rnnlm_data}
     fi
@@ -129,11 +138,11 @@ if [ $stage -le 9 ]; then
         ${rnnlm_dir} ${wordlist} ${rnnlm_data}
 fi
 
-if [ $stage -le 10 ]; then
+if [ $stage -eq 10 ]; then
     ./rescoring/rescore_pruned.sh --ngram-order ${rescore_ngram_order} ${lm} ${rnnlm_dir} ${rnnlm_test} ${decode_og} ${decode_rnnlm}
 fi
 
-if [ $stage -le 11 ]; then
+if [ $stage -eq 11 ]; then
     ./steps/nnet3/report/generate_plots.py --is-chain true ${mdl} ${mdl}/report/
     # copy report locally
     # scp syl20@172.21.150.75:/home/syl20/kaldi-gc/kaldi_egs/commonvoice_spanish/s5/experiments/es/commonvoice/exp/chain/tdnnf_tedlium/report/{log_probability_output,log_probability_output-xent}.pdf .
