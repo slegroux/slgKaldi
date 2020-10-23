b5b0600bd70794949ac77bf9059bf0fc7ccfb62f
Sylvain Le Groux <slegroux@ccrma.stanford.edu>
2020-10-23 04:07:58 +0000

    tcp client/server for online decoding

--- asr/s5/server/test_tcp_server.sh
+++ asr/s5/server/test_tcp_server.sh
@@ -1,3 +1,13 @@
 #!/usr/bin/env bash
-wav=/data/librifrench/test/13/1410/13-1410-0001.wav
-sox $wav -t raw -c 1 -b 16 -r 16k -e signed-integer - | nc localhost 5050
+# (c) 2020 Sylvain Le Groux <slegroux@ccrma.stanford.edu>
+
+port_num=5050
+
+. utils.sh
+. path.sh
+. utils/parse_options.sh
+
+# wav=/data/librifrench/test/13/1410/13-1410-0001.wav
+# wav=/home/syl20/data/en/free-spoken-digit-dataset/recordings/0_george_32.wav 
+wav=$1
+sox $wav -t raw -c 1 -b 16 -r 16k -e signed-integer - | nc localhost ${port_num}
