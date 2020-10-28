dc96c4d4d1e6c9ee616b3deba217c3b6e5da523e
Sylvain Le Groux <slegroux@ccrma.stanford.edu>
2020-10-28 07:52:58 +0000

    cleanup

--- asr/s5.bk/data_prep/format_data.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-# (c) 2020 Sylvain Le Groux <slegroux@ccrma.stanford.edu>
-
-. utils.sh
-. path.sh
-. utils/parse_options.sh
-
-# conda activate slgasr
-./format_commonvoice.py --lang='es' /home/syl20/data/es/commonvoice/test.tsv /tmp/test
